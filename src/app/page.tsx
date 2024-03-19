757b17f5a585f45d0d5050d3d732f1d59a6f0383
deepakbagati <deepakbagati2708@gmail.com>
2024-03-20 02:58:21 +0530

    fixes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-
-
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased
   bg-grid-white/[0.02]">
    <h1 className="text-2xl text-center">Devin Here</h1>
+   <HeroSection />
+   <FeaturedCourses />
+   <WhyChooseUs/>
+   <MusicSchoolTestimonialCards/>
+   <UpcomingWebinars/>
+   <Instructors />
+   <Footer/>
+  
+   
    </main>
   );
 }
